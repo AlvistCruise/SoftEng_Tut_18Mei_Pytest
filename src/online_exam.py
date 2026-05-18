fa9f2c9c0966a2dd6f615feb1cb5ed2fff498ce2
DeathlyGoshy <136491804+AlvistCruise@users.noreply.github.com>
2026-05-18 14:53:09 +0700

    save

--- src/online_exam.py
+++ src/online_exam.py
@@ -1,24 +1,14 @@
 def compute_online_exam(exam_max_duration, accomodation_status, dc_duration, login_time, is_public_holiday):
     if login_time < 1:
         return "Rejected"
-    # Rule 1: Max duration limits
     if exam_max_duration > 1:
         return "Error"
         
-    # Rule 2: Public holidays cancel the exam
-    
-        
-    # Rule 3: Late logins are rejected
-    
-        
-    # Rule 4: Long disconnects trigger auto-submit
     
     if is_public_holiday:
         return "Cancel"
     
     if dc_duration > 5:
         return "autosubmit"
-        
-    # If no special conditions are triggered, the exam submits normally
     else: 
         return "Submit"
