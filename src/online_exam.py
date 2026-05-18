2d51deb2de98bcbcabcb399e74ff4d5802270a7d
saltykimm <124378175+saltykimm@users.noreply.github.com>
2026-05-18 14:34:02 +0700

    FIxed
    All code leads to green. GREEN

--- src/online_exam.py
+++ src/online_exam.py
@@ -6,4 +6,25 @@ def compute_online_exam(
         login_time,
         is_public_holiday
 ):
-    return -1
+        if exam_max_duration < 120:
+                pass
+        elif exam_max_duration > 120:
+                return "Error"
+        
+        if dc_duration < 5:
+                pass
+        elif dc_duration > 5:
+                return "autosubmit"
+        
+        if(login_time < 30):
+                pass
+        elif login_time > 30:
+                return "Rejected"
+        
+        if( not is_public_holiday):
+                pass
+        elif is_public_holiday:
+                return "Cancel"
+        
+        return "Submit"
+        
