ad7aecaedddc55d16dcd1208f9872d98295c2f1e
AndDew8736 <amob8736@gmail.com>
2026-05-18 14:36:21 +0700

    Sugondese

--- src/online_exam.py
+++ src/online_exam.py
@@ -3,7 +3,7 @@ def compute_online_exam(
         exam_max_duration,
         accomodation_status,
         dc_duration,
-        login_time,
+        Sugma,
         is_public_holiday
 ):
     return -1
