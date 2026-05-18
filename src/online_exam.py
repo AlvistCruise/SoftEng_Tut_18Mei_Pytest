9f998420c586e582d98aa5ae7a3196e67e121387
DeathlyGoshy <136491804+AlvistCruise@users.noreply.github.com>
2026-05-18 14:38:43 +0700

    next

--- src/online_exam.py
+++ src/online_exam.py
@@ -3,7 +3,7 @@ def compute_online_exam(
         exam_max_duration,
         accomodation_status,
         dc_duration,
-        login_time,
+        logiaowkoakoakokwaokawoawkoawkoawkowakoawk,
         is_public_holiday
 ):
     return 0AOWKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
